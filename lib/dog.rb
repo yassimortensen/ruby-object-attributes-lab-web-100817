c1f402ad8ded900a67c54601951e92da70eec912
yassimortensen <yassimortensen@gmail.com>
2017-10-11 15:06:38 -0400

    passed all tests

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+
+class Dog
+
+  def name=(dog_name) #setter method
+    @name = dog_name
+  end
+
+  def name #getter method
+    @name
+  end
+
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+
+  def breed
+    @breed
+  end
+
+end
